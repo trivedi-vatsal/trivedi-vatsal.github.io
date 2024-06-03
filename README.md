7e2eae5c65df0885da64aa76af9a860dd9410e25
trivedi-vatsal <trivedivatsal005@gmail.com>
2024-06-03 20:37:07 +0530

    Add : Portfolio

---
+++ README.md
@@ -0,0 +1,77 @@
+# Vatsal Trivedi - Portfolio
+
+This is my portfolio site built with the following technologies. The live URL is [https://vatsal.xyz](https://vatsal.xyz).
+
+## Design
+
+![View 1](public/View1.png)
+
+## Technologies Used
+
+This project utilizes the following technologies:
+
+- **Astro**: A modern framework for building fast and efficient websites.
+- **Tailwind CSS**: A utility-first CSS framework for rapid and responsive design.
+- **Shadcn UI**: A collection of accessible and easy-to-use UI components.
+- **React**: For interactive components and advanced functionality.
+
+## Requirements
+
+Before starting, make sure you have the following requirements installed:
+
+- Node.js (version 14 or higher)
+- npm or yarn
+
+## Installation
+
+To install and run the project locally, follow these steps:
+
+1. Install the dependencies:
+
+   ```bash
+   npm install
+   # or
+   yarn install
+   ```
+
+2. Start the development server:
+
+   ```bash
+   npm run dev
+   # or
+   yarn dev
+   ```
+
+3. Open your browser and visit `http://localhost:4321` to see your portfolio in action.
+
+## Project Structure
+
+A brief overview of the project structure:
+
+- **/public**: Images and other static files.
+- **/src**: Contains all Astro components and pages.
+- **/styles**: Custom CSS files.
+- **astro.config.mjs**: Main configuration for Astro.
+- **tailwind.config.js**: Configuration for Tailwind CSS.
+
+## Important Considerations
+
+- **Accessibility**: Ensure your portfolio is accessible to all users, including those with disabilities.
+- **SEO**: Optimize your portfolio for search engines by adding meta tags and relevant content.
+- **Performance**: Use modern web development practices to ensure optimal performance, such as lazy loading images and minimizing CSS/JS.
+- **Responsive Design**: Make sure your portfolio looks good on devices of all sizes, from mobile to desktop screens.
+
+## Contributions
+
+Contributions are welcome! If you have ideas or improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+This template, originally developed by [bue221](https://github.com/bue221/astro-portfolio), has been tailored to reflect my experience.
+Feel free to reach out with any questions or suggestions to improve this project!
+
+---
---
+++ src/lib/constants.ts
@@ -0,0 +1,104 @@
+import type { Site, Page } from './types'
+
+export const loaderAnimation = [
+  '.loader',
+  { opacity: [1, 0], pointerEvents: 'none' },
+  { easing: 'ease-out' },
+]
+
+export const LINKS = {
+  github: 'https://github.com/trivedi-vatsal',
+  linkedin: 'https://www.linkedin.com/in/trivedi-vatsal/',
+  mail: 'mailto:trivedivatsal005@gmail.com',
+  instagram: 'https://www.instagram.com/trivedivatsal_/',
+  behance: 'https://behance.net/trivedivatsal',
+  dribbble: 'https://dribbble.com/trivedivatsal',
+  resume: 'https://resume.vatsal.xyz',
+}
+
+// Global
+export const SITE: Site = {
+  TITLE: 'Vatsal Trivedi',
+  DESCRIPTION:
+    'Portfolio, Projects, CV, About Me, Contact, Timezone, Now, Experience',
+  AUTHOR: 'Vatsal Trivedi',
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: 'Work',
+  DESCRIPTION: 'Places I have worked.',
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: 'Blog',
+  DESCRIPTION: 'Writing on topics I am passionate about.',
+}
+
+// Projects Page
+export const PROJECTS: Page = {
+  TITLE: 'Projects',
+  DESCRIPTION: 'Recent projects I have worked on.',
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: 'Search',
+  DESCRIPTION: 'Search all posts and projects by keyword.',
+}
+
+// Study Page
+export const STUDIES = [
+  {
+    title: 'Computer Science',
+    institution: 'Universidad Central',
+    link: 'https://www.ucentral.edu.co/',
+    date: '2019 - 2027',
+  },
+  {
+    title: '...',
+    institution: 'Platzi',
+    link: 'https://platzi.com/',
+    date: '2018 - 2022',
+  },
+  {
+    title: '...',
+    institution: 'Udemy',
+    link: 'https://www.udemy.com/',
+    date: '2018 - 2022',
+  },
+  {
+    title: '...',
+    institution: 'Youtube',
+    link: 'https://www.youtube.com/@midulive',
+    date: '2018 - 2022',
+  },
+  {
+    title: 'Tattoo Artist',
+    institution: 'Tattoo Academy',
+    link: 'https://www.instagram.com/tattoodcacademy/',
+    date: '2024 - 2025',
+  },
+]
+
+export const EXPERIENCE = [
+  {
+    company: 'Comviva',
+    location: 'Benguluru, India',
+    position: 'Technical Lead',
+    start: '2020',
+    link: 'https://comviva.com/',
+    end: 'Current',
+    tasks: [],
+  },
+  {
+    company: 'TCS',
+    location: 'Gandhinagar, India',
+    position: 'Software Enginner',
+    link: 'https://tcs.com',
+    start: '2020',
+    end: '2018',
+    tasks: [],
+  },
+]
